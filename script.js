ce22a9cf8f34280f6e753fbd26b25c29c0487c49
Parth2408 <71397228+Parth2408@users.noreply.github.com>
2020-09-16 21:26:47 +0530

    create index.html for my-project GitHub Pages

---
+++ script.js
@@ -0,0 +1,35 @@
+const socket = io('http://localhost:3000')
+const messageContainer = document.querySelector('.Chat')
+const messageForm = document.getElementById('form')
+const messageInput = document.getElementById('inp')
+
+
+const name = prompt('What is your name?')
+appendMessage('You joined')
+socket.emit('new-user', name)
+
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+  appendMessage(`${name} connected`)
+})
+
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} disconnected`)
+})
+
+messageForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const message = messageInput.value
+  appendMessage(`You: ${message}`)
+  socket.emit('send-chat-message', message)
+  messageInput.value = ''
+})
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
+}
